5a5e2630d6547761787262f3b642d38ad97e3cd8
Tim Lochmüller <tim.lochmueller@hdnet.de>
2023-01-26 13:05:02 +0100

    First ideas

---
+++ bin/custom-database-export.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Lochmueller\CustomDatabaseExport\Command\ExportCommand;
+use Symfony\Component\Console\Application;
+
+$application = new Application();
+
+$command = new ExportCommand();
+$application = new Application();
+$application->add($command);
+$application->setDefaultCommand($command->getName());
+$application->run();
---
+++ src/Command/ExportCommand.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Lochmueller\CustomDatabaseExport\Command;
+
+use Druidfi\Mysqldump\Mysqldump;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\Yaml\Yaml;
+
+
+class ExportCommand extends Command
+{
+    public function __construct(string $name = null)
+    {
+        parent::__construct('export');
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addOption('configuration', 'c', InputOption::VALUE_REQUIRED, 'Configuration file', 'custom-database-export.yaml');
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+
+        $configurationFile = $input->getOption('configuration');
+
+        try {
+            $configuration = Yaml::parseFile($configurationFile)['custom-database-export'];
+        } catch (\Exception $exception) {
+            $output->writeln('Errors by parsing configuration files "' . $configurationFile . '": ' . $exception->getMessage());
+            return Command::INVALID;
+        }
+
+        if (!($configuration['structure']['skip'] ?? false)) {
+            $output->writeln('Start DB structure to structure.sql');
+
+            // Structure
+            $settings = [
+                'no-data' => true,
+                'add-drop-table' => true,
+                'include-tables' => $configuration['structure']['tableInclude'] ?? [],
+                'exclude-tables' => $configuration['structure']['tableExclude'] ?? [],
+            ];
+
+            try {
+                $dumper = new Mysqldump(...$this->addCredentials($configuration, $settings));
+                $dumper->start('structure.sql');
+            } catch (\Exception $exception) {
+                $output->writeln('Could not create structure file: ' . $exception->getMessage());
+                return Command::FAILURE;
+            }
+        }
+
+
+        if (!($configuration['data']['skip'] ?? false)) {
+            $output->writeln('Start DB structure to data.sql');
+
+            // Data
+            $settings = [
+                'no-create-info' => true,
+                'include-tables' => $configuration['data']['tableInclude'] ?? [],
+                'exclude-tables' => $configuration['data']['tableExclude'] ?? [],
+            ];
+
+
+            var_dump($configuration);
+
+
+            try {
+                $dumper = new Mysqldump(...$this->addCredentials($configuration, $settings));
+                $dumper->start('data.sql');
+            } catch (\Exception $exception) {
+                $output->writeln('Could not create data file: ' . $exception->getMessage());
+                return Command::FAILURE;
+            }
+        }
+
+        return Command::SUCCESS;
+    }
+
+    protected function addCredentials(array $configuration, array $settings): array
+    {
+        return [
+            'dsn' => 'mysql:host=' . $this->resolveValue($configuration['connection']['host'] ?? '') . ';dbname=' . $this->resolveValue($configuration['connection']['dbname'] ?? ''),
+            'user' => $this->resolveValue($configuration['connection']['username'] ?? ''),
+            'pass' => $this->resolveValue($configuration['connection']['password'] ?? ''),
+            'settings' => $settings,
+        ];
+    }
+
+    protected function resolveValue(string $value): string
+    {
+        if (str_starts_with($value, 'ENV:')) {
+            return (string) getenv(substr($value, 4));
+        }
+        return $value;
+    }
+
+}
